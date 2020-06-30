74b8cd681864be8ad982c7f01dfdd98a1a53df2c
Reetesh Kumar <51451107+rishureetesh@users.noreply.github.com>
2020-06-30 21:41:39 +0530

    New Year Chaos

---
+++ NewYearChaos.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the minimumBribes function below.
+    static void minimumBribes(int[] q) {
+        int bribes=0,flag=0,k=0;
+        int n=q.length;
+        for(int i=n-1;i>=0;i--){
+            if(q[i]-(i+1)>2){
+                System.out.println("Too chaotic");
+                return;
+            }
+            if(q[i]-2<0)
+                k=0;
+            else
+                k=q[i]-2;
+            for(int j=k;j<i;j++){
+                if(q[j]>q[i]){
+                    bribes++;
+                }
+            }
+            
+        }
+        System.out.println(bribes);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] q = new int[n];
+
+            String[] qItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int qItem = Integer.parseInt(qItems[i]);
+                q[i] = qItem;
+            }
+
+            minimumBribes(q);
+        }
+
+        scanner.close();
+    }
+}
